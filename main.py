4a746c54da75657f1ad2fc68bd19be603c7ee826
Sourav Garai <sourav301@gmail.com>
2026-09-05 14:04:20 +0530

    echo added

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def health():
 @app.get("/chat")
 def chat(name: str = Query(..., description="Your name")):
     return {"reply": f"hello  {name}"}
+
+@app.get("/echo")
+def chat(name: str = Query(..., description="Your name")):
+    return {"reply": f"{name}"}
